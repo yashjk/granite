40833942dea66e4db173a60d014094c73b26c5b4
yash-joshi-vineti <yash.joshi@vineti.com>
2022-06-25 13:29:00 +0530

    Added git hooks, eslint, prettier and rubocop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,6 @@
-ENV['RAILS_ENV'] ||= 'test'
+# frozen_string_literal: true
+
+ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
@@ -7,7 +9,7 @@ class ActiveSupport::TestCase
   parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  #fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
