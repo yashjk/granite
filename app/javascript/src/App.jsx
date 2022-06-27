ee7cd073ce2e91fe55d2d7407614d2c81313541f
yash-joshi-vineti <yash.joshi@vineti.com>
2022-06-27 20:01:20 +0530

    Added API based architecture

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,16 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
-import { setAuthHeaders } from './apis/axios';
-import { initializeLogger } from './common/logger';
+import React, { useEffect, useState } from "react";
+
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+import { setAuthHeaders } from "./apis/axios";
+import { initializeLogger } from "./common/logger";
 
 const App = () => {
-  const [ loading, setLoading ] = () => useState(true);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     initializeLogger();
     setAuthHeaders(setLoading);
   }, []);
 
-  if (loading){
+  if (loading) {
     return <h1>Loading...</h1>;
   }
 
@@ -21,7 +23,7 @@ const App = () => {
         <Route exact path="/about" render={() => <div>About</div>} />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
