3ea5211b080adf0f01ac63b19718dc12803a49c5
yash-joshi-vineti <yash.joshi@vineti.com>
2022-06-28 15:22:44 +0530

    Added ability to display list of tasks

--- app/javascript/src/components/NavBar/index.jsx
+++ app/javascript/src/components/NavBar/index.jsx
@@ -8,19 +8,19 @@ const NavBar = () => (
       <div className="flex h-16 justify-between">
         <div className="flex px-2 lg:px-0">
           <div className="hidden lg:flex">
-            <NavItem name="Todos" path="/" />
+            <NavItem name="Todos" path="/dashboard" />
             <NavItem name="Add" iconClass="ri-add-fill" path="/tasks/create" />
           </div>
         </div>
         <div className="flex items-center justify-end">
           <a
             className="transition focus:outline-none inline-flex cursor-pointer items-center
-             border-b-2 border-transparent px-1 pt-1
-             text-sm font-semibold leading-5 text-bb-gray-600
-             text-opacity-50 duration-150 ease-in-out
-             hover:text-bb-gray-600 focus:text-bb-gray-700"
+              border-b-2 border-transparent px-1 pt-1
+              text-sm font-semibold leading-5 text-bb-gray-600
+              text-opacity-50 duration-150 ease-in-out
+              hover:text-bb-gray-600 focus:text-bb-gray-700"
           >
-            LogOut
+            Log Out
           </a>
         </div>
       </div>
